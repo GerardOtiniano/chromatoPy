6a7b2e378b9c1199a4ead508fad8261a69ba2d53
Yashvardhan Sharma <sharmayash.v2006@gmail.com>
2025-08-04 22:05:32 -0400

    Deleted old src directory

--- src/chromatopy/__main__.py
+++
@@ -1,8 +0,0 @@
-from chromatopy.chromatoPy_front_ui import ChromatoPyApp
-
-def main() -> None:
-    app = ChromatoPyApp("ChromatoPy", "com.GerardOtiniano.chromatopy")
-    app.main_loop()
-
-if __name__ == "__main__":
-    main()
