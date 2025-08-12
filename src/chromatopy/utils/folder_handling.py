fa8b7f2b765d0f255454431c8d7064a4337739aa
Yashvardhan Sharma <sharmayash.v2006@gmail.com>
2025-08-12 01:30:16 -0400

    Updated to incorporate setting functions

--- src/chromatopy/utils/folder_handling.py
+++ src/chromatopy/utils/folder_handling.py
@@ -5,7 +5,6 @@ import pandas as pd
 
 from .GDGT_configuration import load_gdgt_window_data
 from .import_data import numerical_sort_key
-from .GDGT_compounds import *
 
 def folder_handling(folder_path):
     """
